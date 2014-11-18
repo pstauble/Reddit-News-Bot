555316607d85f56033a52c5da59b2c138444e716
Patrick <patrick.stauble@gmail.com>
2014-11-18 21:58:36 +0100

    Updated News Bot
    Slimmed it all down and turned it into an unending loop so it can run on heroku.

--- news_bot.py
+++ news_bot.py
@@ -1,31 +1,60 @@
+###################################################
+################### Modules #######################
+###################################################
 
 import requests as r
 from bs4 import BeautifulSoup
 import time
 import praw
 
+###################################################
+################### Configuration #################
+###################################################
+
 username="10724748"
-password="123456"
+password="password"
 
 sub="worldnews"
 
-used_articles=[]
+r = praw.Reddit(user_agent=user_agent)
 
-print "Initializing..."
+used_articles=[]
+links=["http://www.bbc.com/news","http://www.bbc.co.uk/news/world/middle_east","http://www.bbc.co.uk/news/world/europe/"]
 
 user_agent = ("Reddit News Publication Machine v1.3 by /u/patsoldier") #Every reddit bot has to have this to identify itself
 
-def post(i,link):
-    #suggested links: http://www.bbc.com/news and http://www.bbc.co.uk/news/world/middle_east
+###################################################
+################### Functions #####################
+###################################################
 
-    localtime = time.asctime( time.localtime(time.time()) )
-    print "Local current time :", localtime
-    
-    i=str(i)
-    i=GetStory() 
+
+def login(username,password):
+    r.login(username,password)
+
+def post_news(subreddit,title,article_link):
+    r.submit(subreddit,title,text=None,url=article_link)
+
 
     
-    all_contents=i.get_all()
+def get_all(link):
+    all_contents=[]
+    
+    page=r.get(link).text #This gets the page that you will be scraping.
+    soup=BeautifulSoup(page) #Prepares the "soup" that well be working on in a format bs4 can work with.
+    raw=soup.find_all(id="top-story")[0].span.decompose()
+    
+    all_contents.append(raw.p.get_text(strip=True))
+    all_contents.append(raw.find("a").get_text(strip=True))
+    all_contents.append("http://www.bbc.com" + raw.find("a")["href"])
+    all_contents.append(raw.a["rel"][0])
+    return all_contents
+
+
+
+    
+def post(link):
+    
+    all_contents=get_all(link)
 
     if all_contents[3] not in used_articles:
 
@@ -33,9 +62,8 @@ def post(i,link):
 
             title=all_contents[0]
             link=all_contents[2]
-
+            
             post_news(sub,title,link)
-
             used_articles.append(all_contents[3])
 
             print "Succesfully posted: " +title
@@ -47,91 +75,18 @@ def post(i,link):
     else:
         print "Article " + str(all_contents[3])+" has already been posted to reddit."
     time.sleep(600)
-
-
-
-def post_news(subreddit,title,article_link):
-    r = praw.Reddit(user_agent=user_agent)
-    r.login(username,password)
-    r.submit(subreddit,title,text=None,url=article_link)
-
-
-
-class GetStory:
-
-
-    def get_clickbait_line(self):
-
-        raw=soup.find_all(id="top-story")[0]
-        #This gets the subsection with the information of the "top story".
-        #The find all command finds all the sections with the top story id in the soup.
-        clickbait_line.span.decompose()
-        #The line above deletes the contents of the <span> tag, which we will need for later.
-        return clickbait_line.p.get_text()
-        #this then gets us the contents of the <p> tag which has the clickbait line.
-        #Within p was the span tag, which is why we had to delete it.
-        #Get_text returns the text as a string.
-
-    def get_headline(self):
-        
-        article_headline=soup.find_all(id="top-story")[0]
-        return article_headline.find("a").get_text()
-        #find only gets the first tag with the tag <a> (in this case).
-
-    def get_link(self):
-        
-        article_link=soup.find_all(id="top-story")[0]
-        return "http://www.bbc.com" + article_link.find("a")["href"]
-        #The href command like this, like the rel below, gets us the contents of a tag.
-
-    def get_id(self):
-        article_id=soup.find_all(id="top-story")[0]
-        return article_id.a["rel"]
-
-    def get_all(self):
-        all_contents=[]
-        
-        raw=soup.find_all(id="top-story")[0]
-
-        raw.span.decompose()
-        all_contents.append(raw.p.get_text(strip=True))
-
-        
-        all_contents.append(raw.find("a").get_text(strip=True))
-
-        
-        all_contents.append("http://www.bbc.com" + raw.find("a")["href"])
-
     
-        all_contents.append(raw.a["rel"][0])
+    
+###################################################
+################### Main Loop #####################
+###################################################
 
-        return all_contents
+login(username,password)
 
-
-
-
-for i in range(1000):
-    time.sleep(500)
-    link1="http://www.bbc.com/news"
-    link2="http://www.bbc.co.uk/news/world/middle_east"
-    link3="http://www.bbc.co.uk/news/world/europe/"
-
-    print "Retrieving Link1..."
-    page=r.get(link1).text #This gets the page that you will be scraping.
-    soup=BeautifulSoup(page) #Prepares the "soup" that well be working on in a format bs4 can work with.
-    post(i,link1)
-
-    print "Retrieving Link2..."
-    page=r.get(link2).text #This gets the page that you will be scraping.
-    soup=BeautifulSoup(page) #Prepares the "soup" that well be working on in a format bs4 can work with.
-    post(i+1, link2)
-
-    print "Retrieving Link3..."
-    page=r.get(link2).text #This gets the page that you will be scraping.
-    soup=BeautifulSoup(page) #Prepares the "soup" that well be working on in a format bs4 can work with.
-    post(i+1, link2)
-
-
+while True:
+    for i in links:
+        post(i)
+        
     
     
 
